aaa39d9763ba1dc318315e8d677af830900775f4
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-09-08 17:57:51 -0300

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let reverseWord = '';
+  for (let i = word.length; i >= 0; i--) {
+    reverseWord += word.slice(i, i + 1);
+  }
+  return reverseWord;
 };
 
 // Do not edit below this line
