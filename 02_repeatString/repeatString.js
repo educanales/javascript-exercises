fd1a14a7611e320306b0b07a783a7e323d09053a
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-09-08 15:59:55 -0300

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+  let result = '';
+  if (number < 0) return 'ERROR';
+  for (let i = 0; i < number; i++) {
+    result += word;
+  }
+  return result;
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
