a0ece840179de413c35a8be17cbf683cf42ff2f9
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-09-08 22:59:21 -0300

    First attempt of 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value, ...moreValues) {
+  let index = array.indexOf(value);
+  if (moreValues) console.log(moreValues);
+  let otherIndex = array.indexOf(...moreValues);
+  console.log(otherIndex)
+  let firstHalfArray = array.slice(0, index);
+  // console.log(firstHalfArray)
+  let secondHalfArray = array.slice(index + 1);
+  // console.log(secondHalfArray)
+  let newArray = firstHalfArray.concat(secondHalfArray);
+  // console.log(newArray)
+  return newArray;
 };
 
+
+let myArray = [1, 2 ,3 ,4];
+console.log(removeFromArray(myArray, 3, 4));
+
 // Do not edit below this line
-module.exports = removeFromArray;
+// module.exports = removeFromArray;
