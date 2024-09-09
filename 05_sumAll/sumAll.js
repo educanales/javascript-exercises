af23ab692d52f5af0cc324915c10dd7007244e67
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-09-08 23:51:12 -0300

    Complete 05_sumAll and 06_leapYears

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(firstValue, secondValue) {
+  let sum = 0;
+  if (
+    firstValue < 0 || 
+    secondValue < 0 || 
+    secondValue % 1 !== 0 || 
+    firstValue % 1 !== 0 ||
+    typeof firstValue !== 'number'  ||
+    typeof secondValue !== 'number'
+  ) {
+    return 'ERROR';
+  }
+  if (firstValue < secondValue) {
+    for (let i = firstValue; i <= secondValue; i++) {
+      sum += i;      
+    } 
+  } else {
+      for (let i = secondValue; i <= firstValue; i++) {
+        sum += i; 
+      }
+    }
+  return sum;
 };
+
+sumAll(1, 4)
 
 // Do not edit below this line
 module.exports = sumAll;
