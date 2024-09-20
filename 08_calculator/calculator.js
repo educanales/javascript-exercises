1aa6b7c64df21fe46aa707b45541dc8a0565bb3c
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-09-20 13:50:26 -0300

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total = 1;
+	for (let i = 1; i <= number; i++) {
+    total *= i;
+  }
+  return total;
 };
+console.log(factorial(5));
+
 
 // Do not edit below this line
 module.exports = {
